dc2dbd34afbd232cf0b7e2053c648dee81c90fe8
gruzilkin <gruzmob@gmail.com>
2026-03-27 12:28:56 +0900

    change default max limit on dns records

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ class Settings(BaseSettings):
     cloudflare_api_token: str
     cloudflare_zone_id: str
     dns_record_name: str
-    dns_max_records: int = 100
+    dns_max_records: int = 40
     http_queue_size: int = 100
     well_known_path: str = "/.well-known/pki-validation/gsdv.txt"
