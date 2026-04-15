d2b610ce434782ca36b39c505e508c2779ccfa93
gruzilkin <gruzmob@gmail.com>
2026-04-15 17:32:44 +0900

    Revert "simplify contactemail record"
    This reverts commit a3b48d7533c18021e7842375f24f8ba5753a454c.

--- app/config.py
+++ app/config.py
@@ -5,9 +5,11 @@ class Settings(BaseSettings):
     cloudflare_api_token: str
     cloudflare_zone_id: str
     dns_record_name: str
-    contactemail_record_name: str = "_validation-contactemail"
     dns_max_records: int = 40
     dns_max_age_hours: int = 24
     http_queue_size: int = 100
     well_known_path: str = "/.well-known/pki-validation/gsdv.txt"
 
+    @property
+    def contactemail_record_name(self) -> str:
+        return f"_validation-contactemail.{self.dns_record_name}"
